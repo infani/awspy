621ec5c5e284cc328e0785069375f05efa9eeae1
yhattm <yhattm@gmail.com>
2022-07-26 01:24:39 +0000

    update_item

--- src/dynamodb.py
+++ src/dynamodb.py
@@ -2,7 +2,9 @@ import boto3
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('DevicePushedInfo-vtib2crdejecxibzdt5rlklw44-dev')
-# table.update_item(Key={'mac': '000000000000'},UpdateExpression='SET online = :val1',ExpressionAttributeValues={':val1': False})
+table.update_item(Key={'mac': '000000000000'}, UpdateExpression='SET #online = :val1',
+                  ExpressionAttributeNames={'#online': 'online'},
+                  ExpressionAttributeValues={':val1': False})
 response = table.get_item(Key={'mac': '000000000000'})
 item = response['Item']
 print(item)
