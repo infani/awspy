6126467166d9b4a058efff633b36261ac4d3b4e8
Ben <ben@vivotek.com>
2022-12-05 06:03:11 +0000

    fix version

--- awspy/info.py
+++ awspy/info.py
@@ -3,5 +3,5 @@ import configparser
 def version():
     config = configparser.ConfigParser()
     config.read("pyproject.toml")
-    version = config['tool.poetry']['version']
+    version = config['tool.poetry']['version'].replace('"', "")
     return version
